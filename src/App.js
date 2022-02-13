e776652276006bef517bd82b25c476949876db5d
Nitesh Yadav <nteshxx@gmail.com>
2022-02-13 17:36:00 +0530

    transformed useState counter to redux counter

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { decrement, increment, incrementByAmount, reset } from './redux/counter';
 import './style.css';
 
 function App() {
-  const [count, setCount] = React.useState(0);
+  const { count } = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
 
   return (
     <div className="App">
       <h1>The Count is: {count}</h1>
       <div className='buttons'>
-        <button onClick={() => setCount(count - 1)}>decrement</button>
-        <button onClick={() => setCount(count + 1)}>increment</button>
+        <button onClick={() => dispatch(decrement())}>decrement</button>
+        <button onClick={() => dispatch(reset())}>reset</button>
+        <button onClick={() => dispatch(increment())}>increment</button>
       </div>
+      <div><button onClick={() => dispatch(incrementByAmount(5))}>increment by 5</button></div>
     </div>
   );
 };
